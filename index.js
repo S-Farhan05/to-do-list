33731189ed69e89dbdcb5396051a69d0f10c5614
S-Farhan05 <159949925+S-Farhan05@users.noreply.github.com>
2024-04-21 19:49:36 +0500

    A way to save your task in a line.

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let toDo = [];
+let condition = true;
+while (condition) {
+    let toDoList = await inquirer.prompt([
+        {
+            message: "What do u want to add in your to do list : ",
+            name: "Question1",
+            type: "input",
+        },
+        {
+            message: "Do you have more things to add in your to do  list :",
+            type: "confirm",
+            name: "Question2",
+            default: false,
+        }
+    ]);
+    toDo.push(toDoList.Question1);
+    condition = toDoList.Question2;
+}
+console.log(toDo);
